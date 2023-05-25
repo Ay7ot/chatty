2e911ddc1f8a158019c119f1ab1f966d98fcd5fd
Aydot <ayomidotzee@gmail.com>
2023-05-25 14:42:04 -0700

    Revert "Typewriter effect works"
    This reverts commit 37aa21a28267533cd1a6451109549269f6a2773c.

--- src/components/ConversationPage.tsx
+++ src/components/ConversationPage.tsx
@@ -7,7 +7,7 @@ export default function ConversationPage() {
     const { convoStarted } = useAuth()
     
     return (
-        <div className="self-center w-full">
+        <div className="self-center">
             {convoStarted ? <ChatTabs /> : <PreloadConvo />}
         </div>
     )
